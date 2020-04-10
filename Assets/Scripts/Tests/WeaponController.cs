6454165a9b5af31db44c87197622215f3a1fef3e
Max <mharris382@gmail.com>
2020-04-09 20:13:23 -0400

    Added Weapon Angle Editor with Handles
    -improved gizmos for visualizing weapon angles
    -added custom editor with handles for editing angles and distance in the scene view

--- Assets/Scripts/Tests/WeaponController.cs
+++ Assets/Scripts/Tests/WeaponController.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using Sirenix.OdinInspector;
+#if UNITY_EDITOR
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+#endif
 using UnityEngine;
+using UnityEngine.XR;
 
 public class WeaponController : MonoBehaviour
 {
@@ -18,7 +23,11 @@ public class WeaponController : MonoBehaviour
     private Transform _currTarget;
     private bool _firing;
 
-    private Vector3 CenterAngle => transform.up;
+    public Vector3 CenterAngle
+    {
+        get{return  transform.up;}
+        set { transform.up = value; }
+    }
 
     public void FireAtTarget(Transform target)
     {
@@ -63,19 +72,76 @@ public class WeaponController : MonoBehaviour
             weaponTransform.StopFiring();
         }
     }
+#if UNITY_EDITOR
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.yellow;
+        Gizmos.color = Color.yellow.WithAlpha(0.45f);
+        Handles.color = Color.yellow.WithAlpha(0.5f);
         var angle = ((_coneOfFire - 1) / 2f) * 360f;
         var line =CenterAngle.normalized* _maxRange;
         var pRotated = Quaternion.AngleAxis( angle /2, Vector3.forward) * line;
         var nRotated = Quaternion.AngleAxis(-angle/2, Vector3.forward) * line;
         var pos = transform.position;
-        Gizmos.DrawRay(pos, line);
+        //   Gizmos.DrawRay(pos, line);
+        Handles.DrawWireArc(pos, Vector3.forward , nRotated, angle, _maxRange );
         Gizmos.DrawRay(pos, pRotated);
         Gizmos.DrawRay(pos, nRotated);
 
 
     }
+#endif
 }
+
+#if UNITY_EDITOR
+
+
+[CustomEditor(typeof(WeaponController))]
+public class WeaponControllerEditor : OdinEditor
+{
+    private void OnSceneGUI()
+    {
+        var wep = target as WeaponController;
+        Transform transform = wep.transform;
+        var coneProp = serializedObject.FindProperty("_coneOfFire");
+        var rangeProp = serializedObject.FindProperty("_maxRange");
+        float range = rangeProp.floatValue;
+        float dot = coneProp.floatValue;
+        var angle = ((dot - 1) / 2f) * 360f;
+        var line =wep.CenterAngle.normalized* range;
+        var pRotated = Quaternion.AngleAxis( angle /2, Vector3.forward) * line;
+        var nRotated = Quaternion.AngleAxis(-angle/2, Vector3.forward) * line;
+        var pos = transform.position;
+        Handles.color = Color.yellow;
+        Handles.DrawLine(pos, pos + pRotated);
+        Handles.DrawLine(pos, pos + nRotated);
+        Handles.DrawWireArc(pos, Vector3.forward , nRotated, angle, range );
+   
+        var handlePos = pos + line;
+        var handleSize = HandleUtility.GetHandleSize(pos) * 0.035f;
+
+        var prevDist = Vector2.Distance(pos, handlePos);
+        var newPos = Handles.Slider(handlePos, wep.CenterAngle.normalized, handleSize, Handles.DotHandleCap, 0);
+        var newDist = Vector2.Distance(pos, newPos);
+        if (Math.Abs(newDist - prevDist) > Mathf.Epsilon)
+        {
+            rangeProp.floatValue = newDist;
+            return;
+        }
+        var rotation = Quaternion.LookRotation(nRotated, Vector3.forward);
+        Handles.color = Color.clear;
+         
+        var newRotation =Handles.Disc(rotation, pos, Vector3.forward, Mathf.Max(range-1,0.1f), true, 0);
+        if (newRotation != rotation)
+        {
+            var center = Quaternion.LookRotation(wep.CenterAngle, Vector3.forward);
+            var newAngle = Quaternion.Angle(center, newRotation);
+            coneProp.floatValue = Quaternion.Dot(center, newRotation);
+        }
+        //Handles.color = Color.yellow;
+        
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
