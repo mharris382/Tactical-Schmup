0ee8760f0038ed227a1318be426c5aacc2984ba6
Max <mharris382@gmail.com>
2020-04-15 02:17:52 -0400

    Stupid Enemies added

---
+++ Assets/Scripts/Enemies/StupidEnemy.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Enemies
+{
+    public  class StupidEnemy : MonoBehaviour
+    {
+        [Required]
+        public RtsShip ship;
+
+        public float engagementRange = 10;
+        public Transform currentTarget;
+
+        public virtual Transform GetCurrentTarget()
+        {
+            return currentTarget;
+        }
+
+
+        private void Update()
+        {
+            currentTarget = GetCurrentTarget();
+
+            if (currentTarget == null)
+            {
+                ship.LookTarget = ship.transform.position + ship.transform.up;
+                ship.MoveTarget = ship.transform.position;
+                return;
+            }
+
+
+            ship.LookTarget = currentTarget.position;
+            var dist = Vector2.Distance(ship.transform.position, currentTarget.position);
+            if (dist > engagementRange)
+            {
+                var dirToTarget =(ship.transform.position - currentTarget.position).normalized;
+                dirToTarget *= engagementRange;
+                ship.MoveTarget = currentTarget.position + dirToTarget;
+            }
+            else
+            {
+                ship.MoveTarget = ship.transform.position;
+            }
+        }
+    }
+}
