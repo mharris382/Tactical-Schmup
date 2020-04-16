734eef5b92819e1f4dbe49668f223b0c5274b25c
Max <mharris382@gmail.com>
2020-04-15 22:24:14 -0400

    Reorganizing Project - Cleaning Up Prototypes

---
+++ Assets/Scripts/Ships/Defenses/ShipDamageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ships.Defenses
+{
+    public class ShipDamageHandler : MonoBehaviour, IDamageable
+    {
+        
+        public void TakeDamage(DamageInfo damage)
+        {
+           
+        }
+    }
+}
