46353c651a1d53b8b6531d71da9abceae64b81f3
Max <mharris382@gmail.com>
2020-04-20 23:46:36 -0400

    Moved Extensions Methods from GameManager to its own file
    updated 3D test scene to use new stars-skybox

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -11,27 +11,3 @@ namespace Core
         }
     }
 }
-
-
-
-public static class ExtensionMethods
-{
-    public static Color WithAlpha(this Color color, float alpha)
-    {
-        var c = color;
-        c.a = alpha;
-        return c;
-    }
-
-
-    public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
-    {
-        var comp = gameObject.GetComponent<T>();
-        if (comp == null)
-        {
-            comp = gameObject.AddComponent<T>();
-        }
-
-        return comp;
-    }
-}
