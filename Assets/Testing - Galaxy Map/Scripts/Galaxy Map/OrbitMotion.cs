0065227238d87a59bf8d940f49230b40d6ffde55
ChiefTripodBear <avalacos@gmail.com>
2020-04-21 22:12:58 +1000

    Everything is in the Testing - Galaxy Map folder in Assets
    Ive got a test scene with an orbit script and a test planet - Kerodar, the main planet in the system.

---
+++ Assets/Testing - Galaxy Map/Scripts/Galaxy Map/OrbitMotion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OrbitMotion : MonoBehaviour
+{
+    public Transform orbitingObject;
+    public Ellipse orbitPath;
+    
+    [Range(0f,1f)] public float orbitProgress = 0f;
+    public float orbitPeriod = 3f;
+    public bool orbitActive = true;
+
+
+    void Start()
+    {
+        if (orbitingObject == null)
+        {
+            orbitActive = false;
+            return;
+        }
+        else
+        {
+            SetOrbitingObjectPosition();
+            StartCoroutine(AnimateOrbit());
+        }
+    }
+
+    private void SetOrbitingObjectPosition()
+    {
+        Vector2 orbitPos = orbitPath.Evaluate(orbitProgress);
+        orbitingObject.localPosition = new Vector3(orbitPos.x, 0f, orbitPos.y);
+    }
+
+    IEnumerator AnimateOrbit()
+    {
+        if (orbitPeriod < 0.5f)
+        {
+            orbitPeriod = 0.5f;
+        }
+        float orbitSpeed = 1f / orbitPeriod;
+        while (orbitActive)
+        {
+            orbitProgress += Time.deltaTime * orbitSpeed;
+            orbitProgress %= 1f;
+            SetOrbitingObjectPosition();
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Testing - Galaxy Map/Scripts/Galaxy Map/OrbitRenderer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class OrbitRenderer : MonoBehaviour
+{
+    [Range(3, 36)] public int segments;
+    public Ellipse ellipse;
+
+    private LineRenderer lr;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+        CalculateEllipse();
+    }
+
+    private void CalculateEllipse()
+    {
+        Vector3[] points = new Vector3[segments + 1];
+        for (int i = 0; i < segments; i++)
+        {
+            float angle = ((float)i / (float)segments) * 360 * Mathf.Deg2Rad;
+            Vector2 position2D = ellipse.Evaluate((float)i / (float)segments);
+            points[i] = new Vector3(position2D.x, position2D.y, 0f);
+        }
+        points[segments] = points[0];
+
+        lr.positionCount = segments + 1;
+        lr.SetPositions(points);
+    }
+
+    private void OnValidate()
+    {
+        if (Application.isPlaying && lr != null)
+        {
+            CalculateEllipse();
+        }
+    }
+}
